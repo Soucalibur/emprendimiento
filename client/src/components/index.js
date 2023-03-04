aa5cde0d527000d43dff10dcda1fc37c23c7e261
Matthew Gallar <matthewgallar2112@outlook.com>
2023-03-04 01:35:55 -0300

    Agregando estilos

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,6 +6,15 @@ import Slider from "../helper/carousel"
 const Index = ()=>{
 
     LenisFunction()
+
+    const show_hide = (event)=>{
+        const element = document.getElementById(event.target.value)
+        console.log("ELEMENT: ",element.hidden)
+        if(element.hidden === true){
+            element.hidden = false
+        }
+        
+    }
     
     return(
         <div className="containerIndex" >
@@ -16,13 +25,15 @@ const Index = ()=>{
                 <Slider></Slider>
 
             </div>
-            <div className="secondBlock" id="servicios">
+            <div className="secondBlock" id="servicios" >
                 <h3>Servicios</h3>
-                <p>Mas informacion para agregar</p>
+                <button onClick={show_hide}  value="showOrHide">Mas informacion para agregar acaaaaa</button>
+                <p id="showOrHide" hidden> informacion extra que deberia mostrarte u ocultarse a medida que se ven los demas</p>
             </div>
-            <div className="thirdBlock"  id="contacto">
+            <div className="thirdBlock"  id="contacto" >
                 <h3>Contactenos</h3>
-                <p>Mas informacion para agregar</p>
+                <button onClick={show_hide}  value="showOrHide2">Mas informacion para agregar</button>
+                <p id="showOrHide2" hidden>Quizas esto no sea necesario ocultarlo en una primera intancia pero ya se verá</p>
             </div>
             <div className="fourthBlock">
                 <h3>Informacion de los que pertenecen a la página</h3>
